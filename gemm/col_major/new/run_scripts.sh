62ee642111e21c249b4c2f10d63ae98c4114b793
realtyxxx <841344563@qq.com>
2023-03-21 19:11:07 +0800

    changed scripts

--- gemm/col_major/new/run_scripts.sh
+++ gemm/col_major/new/run_scripts.sh
@@ -20,14 +20,13 @@ echo "${runlog}"
 
 # exit
 # 循环三个变量
-for ((m=m_start; m<=m_end; m+=m_step)); do
-  for ((n=n_start; n<=n_end; n+=n_step)); do
-    for ((k=k_start; k<=k_end; k+=k_step)); do
-      # 在最内层循环中执行程序
-      echo -n "Running program with m=$m n=$n k=$k" >> ${runlog}
-      ./test_block 256 256 256 1 0 $m $n $k 0 >> "${runlog}"
-      # 在这里添加你要执行的程序
-    done
-  done
+for ((m = m_start; m <= m_end; m += m_step)); do
+	for ((n = n_start; n <= n_end; n += n_step)); do
+		for ((k = k_start; k <= k_end; k += k_step)); do
+			# 在最内层循环中执行程序
+			echo -n "Running program with m=$m n=$n k=$k" >>${runlog}
+			./test_block.out 256 256 256 1 0 $m $n $k 0 >>"${runlog}"
+			# 在这里添加你要执行的程序
+		done
+	done
 done
-  
