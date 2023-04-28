3d5c96b1f9c68eb205d1a1e6cdaa5c3606ae0f5f
realtyxxx <841344563@qq.com>
2023-04-28 14:38:14 +0800

    add some implementations

--- activation/impl.cc
+++ activation/impl.cc
@@ -19,6 +19,21 @@ enum Activation {
   sigmoid
 };
 
+inline svfloat32_t svtanh_f32_z(svbool_t pg, svfloat32_t val)
+{
+    const svfloat32_t CONST_1        = svdup_n_f32(1.f);
+    const svfloat32_t CONST_2        = svdup_n_f32(2.f);
+    const svfloat32_t CONST_MIN_TANH = svdup_n_f32(-10.f);
+    const svfloat32_t CONST_MAX_TANH = svdup_n_f32(10.f);
+
+    svfloat32_t x     = svmin_f32_z(pg, svmax_f32_z(pg, val, CONST_MIN_TANH), CONST_MAX_TANH);
+    svfloat32_t exp2x = svexp_f32_z(pg, svmul_f32_z(pg, CONST_2, x));
+    svfloat32_t num   = svsub_f32_z(pg, exp2x, CONST_1);
+    svfloat32_t den   = svadd_f32_z(pg, exp2x, CONST_1);
+    svfloat32_t tanh  = svdiv_f32_z(pg, num, den);
+    return tanh;
+}
+
 void leaky_relu_kernel(svfloat32_t src, svbool_t pg, svfloat32_t dst, float alpha,
                 float beta) {}
 
